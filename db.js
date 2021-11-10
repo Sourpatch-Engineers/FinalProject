07d0898d5687f10d131bb114caa22502cf83d0f5
Bryan Gabe <bryantgabe@lewisu.edu>
2021-11-09 19:55:05 -0600

    Added in a loadMetaData that will return a file based on team name

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 
 const { MongoClient } = require('mongodb')
-
-
+const uri = "mongodb+srv://app_user:ibn9j5dxCFfiuBka@cluster0.ohmbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri)
+/**
+ * function will handle the connection with mongodb
+ */
 async function main() {
-  const uri = "mongodb+srv://app_user:ibn9j5dxCFfiuBka@cluster0.ohmbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  const client = new MongoClient(uri)
+
 
   try {
     await client.connect()
 
-    await listDB(client)
+    //await listDB(client)
+    await loadMetaData('sourpatch engineers')
   } catch(e) {
     console.error(e)
   } finally {
@@ -17,13 +20,28 @@ async function main() {
   }
 }
 
-main().catch(console.error)
+main()
 
 
-async function listDB(client) {
+async function listDB() {
   const dbList = await client.db().admin().listDatabases()
   console.log("DB: ")
   dbList.databases.forEach( db => {
     console.log(`- ${db.name}`)
   })
+}
+
+/**
+ * 
+ * @param {string} teamname the team you are searching for located in the database
+ * @returns the team bson file within the db using the query of teamname
+ * @todo will be ids in the future rather than names
+ */
+async function loadMetaData(teamname) {
+  const db = client.db('team_meta')
+  const metatable = db.collection('metatable')
+
+  const query = {team_name: teamname}
+  const team = await metatable.findOne(query)
+  return team
 }

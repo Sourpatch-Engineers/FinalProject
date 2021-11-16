d24d9da1f455d6f3ac20429a537249e13fa5f513
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2021-11-16 17:35:58 -0600

    adding single team info page

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/team_form', handlers.team_form)
 app.get('/about', handlers.about)
 
 app.get('/view_teams', handlers.view_teams)
+app.get('/view_teams/:teamName', handlers.view_one_team)
 app.get('/login', handlers.login)
 
 app.listen(port, () => {
